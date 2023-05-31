95c03b6596275f40555e2a6972927750beb02976
BBalsara <43838069+BBalsara@users.noreply.github.com>
2023-05-30 19:20:57 -0700

    Coded EKF for linear, sine, and DDR trajectories

--- simulation.py
+++ simulation.py
@@ -1,7 +1,8 @@
 # code for simulation and running tests
-import utilities as util
+# import utilities as util
 import numpy as np
-
+from utilities import Movement, Sensor, World
+import matplotlib.pyplot as plt
 # Simulation class
 
 
@@ -9,14 +10,126 @@ import numpy as np
 # Cameras (Position, FOV)
 fov = np.radians(120)
 bearing1 = np.radians(45) 
-bearing2 = np.radians(225) 
+bearing2 = np.radians(225)
+bearing3 = np.radians(135)
+bearing4 = np.radians(180) 
 position1 = np.array([0,0])
 position2 = np.array([10,10])
-dist_noise = 0.1
+position3 = np.array([15,0])
+position4 = np.array([30,5])
 angle_noise = 0.1
-sensor1 = util.Sensor(position1, fov, bearing1, dist_noise, angle_noise)
-sensor2 = util.Sensor(position2, fov, bearing2, dist_noise, angle_noise)
-world = util.World(np.array([sensor1, sensor2]))
-world.FOVplot()
+sensor1 = Sensor(position1, fov, bearing1, angle_noise)
+sensor2 = Sensor(position2, fov, bearing2, angle_noise)
+sensor3 = Sensor(position3, fov, bearing3, angle_noise)
+sensor4 = Sensor(position4, fov, bearing4, angle_noise)
+world = World(np.array([sensor1, sensor2, sensor3, sensor4]))
+render, ax = world.FOVplot()
 
-#
+
+# EKF prediction
+def predict(prev_mu, prev_cov, target: Movement, u):
+    pred_mu = target.f_function(mu = prev_mu, u = u)
+    A = target.jacobian_A(mu = prev_mu, u = u)
+    pred_cov = A @ prev_cov @ A.T + target.Q
+    return pred_mu, pred_cov
+
+# EKF update
+def update(target: Movement, pred_mu, pred_cov, world: World):
+    # pull out sensor data
+    sensors = [sensor for sensor in world.sensors if sensor.is_visible(target.state)]
+    sensor_count = len(sensors)
+    true_state = target.state
+    sensor_noises = np.zeros(sensor_count) # for storing noise values of sensors with target in FOV
+    C = np.zeros((sensor_count, len(true_state))) # for storing jacobian values of sensors with target in FOV
+    y = np.zeros(sensor_count) # for storing true measurements of sensors with target in FOV
+    g = np.zeros(sensor_count) # for storing predicted measurements of sensors with target in FOV
+    for idx, sensor in enumerate(sensors):
+        C[idx,:] = sensor.jacobian_C(pred_mu)
+        sensor_noises[idx] = sensor.angle_noise
+        y[idx] = sensor.angle_meas(true_state)
+        g[idx] = sensor.g(pred_mu)
+    if sensor_count == 0:
+        return pred_mu, pred_cov
+    else:
+        Kt = pred_cov @ C.T @ np.linalg.inv(C @ pred_cov @ C.T + np.diag(sensor_noises))
+        mu = pred_mu + Kt @ (y-g)
+        cov = pred_cov - Kt @ C @ pred_cov 
+
+    return mu, cov
+
+
+Tmax = 40
+d_t = 0.01
+mu = np.zeros((3, int(Tmax/d_t)))
+cov = np.zeros((3, 3, int(Tmax/d_t)))
+mu0 = np.array([4.5,0,0])
+cov0 = 0.01*np.eye(3)
+mu[:, 0] = mu0
+cov[:, :, 0] = cov0
+
+trueInit = np.array([5,0,0])
+trueState = np.zeros((3, int(Tmax/d_t)))
+trueState[:, 0] = trueInit
+
+# Initialize the object
+Object_a = Movement(Movement_pattern='diff_drive', del_t=d_t, initial_location=trueInit)
+
+
+for t in range(1, int(Tmax/d_t)):
+    # Simulation
+    prev_mu = mu[:, t-1]
+    prev_cov = cov[:, :, t-1]
+
+    # Calculate u if needed
+    u = np.array([0.8, np.sin(t*d_t)])
+
+    # Update true State
+    x = Object_a.one_step(u)
+    # print("x: ", x)
+    trueState[:, t] = x
+    
+    # Predict step
+    pred_mu, pred_cov = predict(prev_mu, prev_cov, Object_a, u)
+
+    # Update step
+    updated_mu, updated_cov = update(Object_a, pred_mu, pred_cov, world)
+
+    # Store values
+    mu[:, t] = updated_mu
+    cov[:, :, t] = updated_cov
+    
+# Plotting
+timeArray = np.arange(0,Tmax,d_t)
+plt.figure()
+plt.plot(timeArray,trueState[0,:], label='true px')
+plt.plot(timeArray,mu[0,:], label='estimated px')
+plt.legend()
+plt.title('EKF Output for px')
+plt.xlabel('time')
+plt.ylabel('px')
+# plt.show()
+
+plt.figure()
+plt.plot(timeArray,trueState[1,:], label='true py')
+plt.plot(timeArray,mu[1,:], label='estimated py')
+plt.legend()
+plt.title('EKF Output for py')
+plt.xlabel('time')
+plt.ylabel('py')
+# plt.show()
+
+ax.plot()
+ax.plot(trueState[0,:],trueState[1,:], label='true path')
+ax.plot(mu[0,:],mu[1,:], label='estimated path')
+ax.legend()
+ax.set_title('EKF Output for path')
+ax.set_xlabel('px')
+ax.set_ylabel('py')
+
+plt.figure()
+plt.plot(sensor2.history, label='sensor1')
+plt.legend()
+plt.title('Sensor1 history')
+plt.xlabel('time')
+plt.ylabel('angle')
+plt.show()
--- utilities.py
+++ utilities.py
@@ -4,84 +4,87 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
 class Sensor():
-    def __init__(self, position, fov, bearing, dist_noise, angle_noise):
+    def __init__(self, position, fov, bearing, angle_noise):
         self.position = position # position of the sensor in global frame
         self.bearing = bearing # angle definining center of FOV from global frame x-axis
         self.fov = fov # total angle of view for the camera (split in two across the bearing)
-        self.dist_noise = dist_noise # should be larger
+        # self.dist_noise = dist_noise # should be larger
         self.angle_noise = angle_noise # should be smaller
+        self.history = np.array([])
     
     # returns true if the target is visible to the sensor
     def is_visible(self, target_pos):
         # Relative position
-        rel_pos = target_pos - self.position
+        rel_pos = target_pos[0:2] - self.position
 
         # Angle between sensor and target
-        meas_angle = np.arctan2(rel_pos[1], rel_pos[0]) - self.bearing
+        meas_angle = self.g(target_pos)
 
         return (-self.fov/2 <= meas_angle and meas_angle <= self.fov/2)
 
     # returns the measurement of the object's distance in the sensor frame
-    def dist_meas(self, target_pos):
-        dist = np.linalg.norm(self.position - target_pos)
-        # dist += np.random.normal(0, self.dist_noise)
+    # def dist_meas(self, target_pos):
+    #     dist = np.linalg.norm(self.position - target_pos)
+    #     # dist += np.random.normal(0, self.dist_noise)
 
-        return dist
+    #     return dist
     
     # returns the measurement of the object's angle in the sensor frame
     def angle_meas(self, target_pos):
-        # Angle between sensor and target
-        rel_pos = target_pos - self.position
-        angle = np.arctan2(rel_pos[1], rel_pos[0]) - self.bearing
-        # angle += np.random.normal(0, self.angle_noise)
-
-        return angle   
+        meas = self.g(target_pos) + np.random.normal(0, self.angle_noise)
+        self.history = np.append(self.history, meas)
+        return meas   
     
     # sensor measurement model return (dist, theta)
     def g(self, target_pos):
-        return np.array([self.dist_meas(target_pos), self.angle_meas(target_pos)])
+        # Angle between sensor and target
+        rel_pos = target_pos[0:2] - self.position
+        angle = ((np.arctan2(rel_pos[1], rel_pos[0])) + 2*np.pi)%(2*np.pi) - self.bearing
+        return angle
     
     # sensor measurement model with noise
     def measurment(self, target_pos):
-        return self.g(target_pos) + np.array([np.random.normal(0, self.dist_noise), np.random.normal(0, self.angle_noise)])
+        return self.g(target_pos) + np.array([np.random.normal(0, self.angle_noise)])
 
     # returns the measurement of the object in the global frame (Not needed)
-    def globalTransform(self, target_pos):
-        if self.isVisible(target_pos):
-            angle = self.angle_meas(target_pos)
-            dist = self.dist_meas(target_pos)
+    # def globalTransform(self, target_pos):
+    #     if self.isVisible(target_pos):
+    #         angle = self.angle_meas(target_pos)
+    #         # dist = self.dist_meas(target_pos)
             
-            xm = dist*np.cos(angle+self.bearing) + self.position[0]
-            ym = dist*np.sin(angle+self.bearing) + self.position[1]
+    #         xm = dist*np.cos(angle+self.bearing) + self.position[0]
+    #         ym = dist*np.sin(angle+self.bearing) + self.position[1]
 
-            return np.array([xm, ym])
+    #         return np.array([xm, ym])
         
     def jacobian_C(self, target_pos):
         Px = target_pos[0]
         Py = target_pos[1]
         Sx = self.position[0]
         Sy = self.position[1]
-        # first row
-        dist = self.dist_meas(target_pos)
-        C11 = (Px - Sx)/dist
-        C12 = (Py - Sy)/dist
+        # firsWt row
+        # dist = self.dist_meas(target_pos)
+        # C11 = (Px - Sx)/dist
+        # C12 = (Py - Sy)/dist
         # second row
-        C21 = (Sy - Py)/(dist**2)
-        C22 = (Px - Sx)/(dist**2)
-        return np.array([C11, C12],[C21,C22])
+        C21 = (Sy - Py)/(np.linalg.norm(self.position - target_pos[0:2])**2)
+        C22 = (Px - Sx)/(np.linalg.norm(self.position - target_pos[0:2])**2)
+        return np.array([C21,C22,0])
+
 
 class World():
     def __init__(self, sensors):
         self.sensors = sensors # list of sensors in the world
 
     # function for adding sensors to the world
-    def addSensor(self, position, FOV, bearing, dist_noise, angle_noise):
-        self.sensors.append(Sensor(position, FOV, bearing, dist_noise, angle_noise))        
+    def addSensor(self, position, FOV, bearing, angle_noise):
+        self.sensors.append(Sensor(position, FOV, bearing, angle_noise))        
         
     # function to plot location of sensors and their respective FOVs
     def FOVplot(self):
         k = 5 # length of arrow scale factor
         fig, ax = plt.subplots()
+        plt.grid()
         for sensor in self.sensors:   
             plt.scatter(sensor.position[0], sensor.position[1], color='k')
             plt.arrow(sensor.position[0], sensor.position[1], k*np.cos(sensor.bearing), k*np.sin(sensor.bearing), color='b', head_width=0.1, linestyle = 'dotted')
@@ -98,53 +101,52 @@ class World():
             y = r*np.sin(theta) + sensor.position[1]
             arcPart = patches.Polygon(np.column_stack((x,y)), color='k', alpha=0.2)
             ax.add_patch(arcPart)
-        plt.grid()
         plt.gca().set_aspect('equal')
-        plt.show()
-
-        
-
+        return fig, ax
 
 class Movement():
-    def __init__(self, Movement_pattern='linear', initial_location=np.array([0, 0]), del_t=0.01, speed=1, angle=0):
+    def __init__(self, Movement_pattern='linear', initial_location=np.array([0, 0]), del_t=0.01, speed=.5, angle=0):
         self.pattern = Movement_pattern.lower()  # can be 'Linear', 'sine', 'diff_drive' 
         self.state = initial_location  # Location in the world frame
+        self.initial_location = initial_location
         self.del_t = del_t
         self.speed = speed
-        self.angle = angle  # angle in rad
+        self.angle = angle
 
         if self.pattern == 'linear':
             self.state = initial_location
+            self.Q = 0.001* np.eye(2)
         elif self.pattern == 'sine' or self.pattern == 'sin':
             self.pattern = 'sine'
             self.state = initial_location
+            self.Q = 0.08* np.eye(2)
         elif self.pattern == 'diff_drive':
+            self.Q = 0.02* np.eye(3)
             if initial_location.shape[0] == 2:
                 self.state = np.hstack((initial_location, angle))
+                self.Q = 0.02* np.eye(3)
 
 
-    def f_function(self, u=np.array([0, 0])):
+    def f_function(self, mu, u=np.array([0, 0])):
         # Linear movement
-        if self.loc == 'linear':
+        if self.pattern == 'linear':
             # x_dot = vel * cos angle
             state = np.array([np.cos(self.angle) * self.speed * self.del_t,
-                                   np.sin(self.angle) * self.speed * self.del_t]) + self.state
+                                np.sin(self.angle) * self.speed * self.del_t]) + mu
             return state
 
         # Sine movement
-        if self.loc == 'sine':
-            # xt = vel * delt * x_t-1
-            # yt = sin(xt) + y_t-1
+        elif self.pattern == 'sine':
             del_xt = self.speed * self.del_t
-            state = np.array([del_xt,
-                                   np.sin(del_xt + self.state[0])]) + self.state
+            state = np.array([del_xt + mu[0],
+                                np.sin(del_xt + mu[0] - self.initial_location[0]) + self.initial_location[1]])
             return state
         
         # Diff drive
-        if self.loc == 'diff_drive':
-            px = self.state[0]
-            py = self.state[1]
-            theta = self.state[2]
+        elif self.pattern == 'diff_drive':
+            px = mu[0]
+            py = mu[1]
+            theta = mu[2]
             v = u[0]
             phi = u[1]
             state = (np.array([px + self.del_t * v * np.cos(theta),
@@ -155,28 +157,27 @@ class Movement():
         print('No Movement Pattern defined')
         return None
     
-    def jacobian_A(self, u=np.array([0, 0])):
+    def jacobian_A(self, mu, u=np.array([0, 0])):
         # Linear movement
-        if self.loc == 'linear':
+        if self.pattern == 'linear':
             return np.eye(2)
         
         # Sine movement
-        if self.loc == 'sine':
+        elif self.pattern == 'sine':
             return np.array([[1, 0],
-                             [np.cos(self.speed * self.del_t + self.state[0]), 1]])
+                            [np.cos(self.speed * self.del_t + mu[0] - self.initial_location[0]), 1]])
         
         # Diff drive
-        if self.loc == 'diff_drive':
-            px = self.state[0]
-            py = self.state[1]
-            theta = self.state[2]
+        elif self.pattern == 'diff_drive':
+            theta = mu[2]
             v = u[0]
-            phi = u[1]
             out = np.array([[1, 0, -self.del_t * v * np.sin(theta)],
                         [0, 1, self.del_t * v * np.cos(theta)],
                         [0, 0, 1]])
-            return out
+        return out
         
     # use this to update the internal state values
     def one_step(self, u=np.array([0, 0])):
-        self.state = self.f_function(u)
+        dims = self.state.shape[0]
+        self.state = self.f_function(self.state, u) + np.random.multivariate_normal(np.zeros(dims), self.del_t*self.Q)
+        return self.state
